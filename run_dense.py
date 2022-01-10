277f5d57c6b27dadf99a1d49eaa203f8c4ce5922
Jamal <bajwa.jamal@gmail.com>
2022-01-10 14:36:58 -0500

    Changed model

--- run_dense.py
+++ run_dense.py
@@ -155,7 +155,7 @@ def run_dense_bnn(gpu=True):
             "Dense_1.bias": dist.Normal(0, 50), 
             "Dense_1.kernel": dist.Normal(0, 50),
             "Dense_2.bias": dist.Normal(0, 10), 
-            "Dense_2.kernel": dist.Normal(0, 10)
+            "Dense_2.kernel": dist.Normal(0, 10),
             "Dense_3.bias": dist.Normal(0, 50), 
             "Dense_3.kernel": dist.Normal(0, 50),
             "Dense_3.bias": dist.Normal(0, 100), 
