54ecd81e948d49deb903ce084c9651b1808cb28b
Jamal <bajwa.jamal@gmail.com>
2022-01-11 19:59:09 -0500

    added simple convolutional net

--- run_cnn.py
+++ run_cnn.py
@@ -126,17 +126,17 @@ def run_dense_bnn(gpu=True):
         @nn.compact
         def __call__(self, x):
             
-        x = nn.Conv(features=64, kernel_size=(3, 3))(x)
-        x = nn.softplus(x)
-        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = nn.Conv(features=128, kernel_size=(3, 3))(x)
-        x = nn.softplus(x)
-        x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
-        x = x.reshape(x.shape[0], -1)  # flatten
-        x = nn.Dense(features=32)(x)
-        x = nn.softplus(x)
-        x = nn.Dense(features=10)(x)
-        x = nn.softmax(x)
+            x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+            x = nn.softplus(x)
+            x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+            x = nn.Conv(features=128, kernel_size=(3, 3))(x)
+            x = nn.softplus(x)
+            x = nn.max_pool(x, window_shape=(2, 2), strides=(2, 2))
+            x = x.reshape(x.shape[0], -1)  # flatten
+            x = nn.Dense(features=32)(x)
+            x = nn.softplus(x)
+            x = nn.Dense(features=10)(x)
+            x = nn.softmax(x)
             
             return x
         
