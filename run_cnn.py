5bf79b13f63457b1f1c2595f53a1ba4cd700cfbf
Jamal <bajwa.jamal@gmail.com>
2022-01-11 20:13:00 -0500

    added simple convolutional net

--- run_cnn.py
+++ run_cnn.py
@@ -126,12 +126,12 @@ def run_dense_bnn(gpu=True):
         @nn.compact
         def __call__(self, x):
             
-            x = nn.Conv(features=32, kernel_size=(3, 3))(x)
+            x = nn.Conv(features=32, kernel_size=(3, 3, 3))(x)
             x = nn.relu(x)
-            x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
-            x = nn.Conv(features=64, kernel_size=(3, 3))(x)
+            x = nn.avg_pool(x, window_shape=(2, 2, 1), strides=(2, 2, 1))
+            x = nn.Conv(features=64, kernel_size=(3, 3, 1))(x)
             x = nn.relu(x)
-            x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+            x = nn.avg_pool(x, window_shape=(2, 2, 1), strides=(2, 2, 1))
             x = x.reshape((x.shape[0], -1))  # flatten
             x = nn.Dense(features=256)(x)
             x = nn.relu(x)
