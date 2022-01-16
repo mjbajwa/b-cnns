0774ce3e43707a1b5ef78d2df3a162f4f2c0d5d7
Jamal <bajwa.jamal@gmail.com>
2022-01-16 10:52:15 -0500

    run cnns

--- run_cnn.py
+++ run_cnn.py
@@ -23,9 +23,11 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=True):
     # Administrative stuff
 
     print(jax.default_backend())
+    print(jax.device_count())
+    
     # Disable tensorflow from using GPU
 
-    tf.enable_v2_behavior()
+    # tf.enable_v2_behavior()
     
     if gpu:
 
