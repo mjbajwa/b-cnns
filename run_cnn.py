1d8472823d477540cf0e3c2157233b00dcf16cea
Jamal <bajwa.jamal@gmail.com>
2022-01-16 16:28:36 -0500

    altered arch

--- run_cnn.py
+++ run_cnn.py
@@ -143,8 +143,8 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
 
     # Initialize MCMC
 
-    kernel = NUTS(model, init_strategy=init_to_value(values=init_new), target_accept_prob=0.70)
-    # kernel = NUTS(model, init_strategy=init_to_feasible(), target_accept_prob=0.80)
+    # kernel = NUTS(model, init_strategy=init_to_value(values=init_new), target_accept_prob=0.70)
+    kernel = NUTS(model, init_strategy=init_to_feasible(), target_accept_prob=0.70)
     mcmc = MCMC(  
         kernel,
         num_warmup=NUM_WARMUP,
