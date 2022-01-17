9bca90d42acca587abcc3e47a3203405eb9eb2c9
Jamal <bajwa.jamal@gmail.com>
2022-01-17 11:09:43 -0500

    changed cnn

--- run_cnn.py
+++ run_cnn.py
@@ -86,7 +86,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
             x = nn.Dense(features=16)(x)
             x = nn.swish(x)
             x = nn.Dense(features=10)(x)
-            x = nn.softmax(x)
+            # x = nn.softmax(x)
                 
             return x
         
@@ -112,7 +112,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
             input_shape=(1, 32, 32, 3)
         )
                 
-        numpyro.sample("y_pred", dist.Multinomial(total_count=1, probs=net(x)), obs=y)
+        numpyro.sample("y_pred", dist.Categorical(logits=net(x)), obs=jnp.argmax(y, axis=1))
 
 
     # Initialize parameters 
@@ -200,7 +200,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser(description="Convolutional Bayesian Neural Networks for CIFAR-10")
-    parser.add_argument("--train_index", type=int, default=50000)
+    parser.add_argument("--train_index", type=int, default=100)
     parser.add_argument("--num_warmup", type=int, default=10)
     parser.add_argument("--num_samples", type=int, default=10)
     parser.add_argument("--gpu", type=bool, default=False)
