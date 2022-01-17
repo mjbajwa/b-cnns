7882f9c13c76e4107f89135f7595c9f9be07fe7e
Jamal <bajwa.jamal@gmail.com>
2022-01-17 11:37:59 -0500

    change data sizes

--- run_cnn.py
+++ run_cnn.py
@@ -117,8 +117,8 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
         )
         
         # numpyro.sample("y_pred", dist.Multinomial(total_count=1, probs=net(x)), obs=y)
-        y1 = np.argmax(y, axis=1)
-        numpyro.sample("y_pred", dist.Categorical(logits=net(x)), obs=y1)
+        # y1 = jnp.argmax(y, axis=0)
+        numpyro.sample("y_pred", dist.Categorical(logits=net(x)), obs=y)
 
 
     # Initialize parameters 
