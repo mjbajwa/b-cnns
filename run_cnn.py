b9744f368b4ac71e968828af0cf0533b93c97185
Jamal <bajwa.jamal@gmail.com>
2022-01-17 12:23:10 -0500

    change data sizes

--- run_cnn.py
+++ run_cnn.py
@@ -30,25 +30,24 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
 
     tf.enable_v2_behavior()
 
-    # if gpu:
+    if gpu:
 
-    #    physical_devices = tf.config.list_physical_devices('GPU')
+        physical_devices = tf.config.list_physical_devices('GPU')
 
-    try:
-        # Disable first GPU
-        tf.config.set_visible_devices(physical_devices[1:], 'TPU')
-        logical_devices = tf.config.list_logical_devices('TPU')
-        # Logical device was not created for first GPU
-        assert len(logical_devices) == len(physical_devices) - 1
-    except:
-        pass
+        try:
+            # Disable first GPU
+            tf.config.set_visible_devices(physical_devices[1:], 'TPU')
+            logical_devices = tf.config.list_logical_devices('TPU')
+            # Logical device was not created for first GPU
+            assert len(logical_devices) == len(physical_devices) - 1
+        except:
+            pass
 
-    # Enable JAX/NumPyro to use GPU
+        # Enable JAX/NumPyro to use GPU
 
-    os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
-    os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".8"
-    numpyro.set_platform("cpu")
-    numpyro.set_host_device_count(95)
+        os.environ["XLA_PYTHON_CLIENT_ALLOCATOR"] = "platform"
+        os.environ["XLA_PYTHON_CLIENT_MEM_FRACTION"] = ".8"
+        numpyro.set_platform("gpu")
 
     np.random.seed(0)
 
@@ -159,7 +158,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
 
     # kernel = NUTS(model, init_strategy=init_to_value(values=init_new), target_accept_prob=0.70)
     kernel = NUTS(model, init_strategy=init_to_feasible(), target_accept_prob=0.70,
-                  max_tree_depth=2)
+                  max_tree_depth=10)
     mcmc = MCMC(
         kernel,
         num_warmup=NUM_WARMUP,
@@ -194,8 +193,6 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
     train_preds = Predictive(model, mcmc.get_samples())(
         jax.random.PRNGKey(2), train_x, y=None)["y_pred"]
     train_preds_ave = jnp.mean(train_preds, axis=0)
-    # print(train_preds_ave)
-    # print(y_train)
     # train_preds_index = jnp.argmax(train_preds_ave, axis=1)
     # accuracy = (temp_ds["label"] == train_preds_index).mean()*100
     accuracy = (y_train == train_preds_ave).mean()*100
@@ -205,14 +202,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
 
     test_preds = Predictive(model, mcmc.get_samples())(
         jax.random.PRNGKey(2), test_x, y=None)["y_pred"]
-    print(test_preds.shape)
     test_preds_ave = jnp.mean(test_preds, axis=0)
-    # test_preds_index = jnp.argmax(test_preds_ave, axis=1)
-    # accuracy = (test_ds["label"] == test_preds_index).mean()*100
-    print(y_test.shape)
-    print(y_test)
-    print(test_preds_ave.shape)
-    print(test_preds_ave)
     accuracy = (y_test == test_preds_ave).mean()*100
     print("Test accuracy: ", accuracy)
 
