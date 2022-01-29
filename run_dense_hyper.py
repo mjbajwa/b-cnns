ca0970f14ad47f6513012872aafa0cb1902034cc
Jamal <bajwa.jamal@gmail.com>
2022-01-28 19:39:43 -0500

    added changes to max tree depth

--- run_dense_hyper.py
+++ run_dense_hyper.py
@@ -163,7 +163,7 @@ def run_dense_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False)
 
     # Initialize MCMC
 
-    kernel = NUTS(model, init_strategy=init_to_feasible(), target_accept_prob=0.70, max_tree_depth=1)
+    kernel = NUTS(model, init_strategy=init_to_feasible(), target_accept_prob=0.80, max_tree_depth=12)
     mcmc = MCMC(  
         kernel,
         num_warmup=NUM_WARMUP,
