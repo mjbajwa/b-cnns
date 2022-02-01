0cbf330d830b9d3c5db6d2e0cf02fa5aeecbafa5
Jamal <bajwa.jamal@gmail.com>
2022-02-01 17:34:29 -0500

    added utils to cnn code

--- run_cnn.py
+++ run_cnn.py
@@ -160,7 +160,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
     kernel = NUTS(model, 
                   init_strategy=init_to_feasible(), 
                   target_accept_prob=0.70,
-                  max_tree_depth=1)
+                  max_tree_depth=11)
     mcmc = MCMC(
         kernel,
         num_warmup=NUM_WARMUP,
