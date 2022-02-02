e8a0c9afe4fbfd4b4d9df1d5f8602895df1ea391
Jamal <bajwa.jamal@gmail.com>
2022-02-02 12:29:46 -0500

    modified arch a little bit

--- run_cnn.py
+++ run_cnn.py
@@ -88,15 +88,16 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
         @nn.compact
         def __call__(self, x):
             x = nn.Conv(features=8, kernel_size=(3, 3))(x)
-            x = nn.swish(x)
-            x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+            x = nn.softplus(x)
+            x = nn.max_pool(x, window_shape=(2, 2)) # strides=(2, 2))
             x = nn.Conv(features=16, kernel_size=(3, 3))(x)
-            x = nn.swish(x)
-            x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
+            x = nn.softplus(x)
+            x = nn.max_pool(x, window_shape=(2, 2)) #, strides=(2, 2))
             x = x.reshape((x.shape[0], -1))  # flatten
             x = nn.Dense(features=64)(x)
-            x = nn.swish(x)
+            x = nn.softplus(x)
             x = nn.Dense(features=10)(x)
+            x = nn.softplus(x)
             x = nn.softmax(x)
             return x
 
@@ -160,7 +161,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
     kernel = NUTS(model, 
                   init_strategy=init_to_feasible(), 
                   target_accept_prob=0.70,
-                  max_tree_depth=11)
+                  max_tree_depth=1)
     mcmc = MCMC(
         kernel,
         num_warmup=NUM_WARMUP,
