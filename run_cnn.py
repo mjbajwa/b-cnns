4600a32df2a0b9dea75ad4980de474ffb4ccf5b9
Jamal <bajwa.jamal@gmail.com>
2022-02-02 15:07:16 -0500

    modified arch a little bit

--- run_cnn.py
+++ run_cnn.py
@@ -89,15 +89,14 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
         def __call__(self, x):
             x = nn.Conv(features=8, kernel_size=(3, 3))(x)
             x = nn.softplus(x)
-            x = nn.max_pool(x, window_shape=(2, 2)) # strides=(2, 2))
+            x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
             x = nn.Conv(features=16, kernel_size=(3, 3))(x)
             x = nn.softplus(x)
-            x = nn.max_pool(x, window_shape=(2, 2)) #, strides=(2, 2))
+            x = nn.avg_pool(x, window_shape=(2, 2), strides=(2, 2))
             x = x.reshape((x.shape[0], -1))  # flatten
             x = nn.Dense(features=64)(x)
             x = nn.softplus(x)
             x = nn.Dense(features=10)(x)
-            x = nn.softplus(x)
             x = nn.softmax(x)
             return x
 
