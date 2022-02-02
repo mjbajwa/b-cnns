879a05e61c153752bf9270153a2eccfff5e60f05
Jamal <bajwa.jamal@gmail.com>
2022-02-02 12:44:55 -0500

    modified arch a little bit

--- run_cnn.py
+++ run_cnn.py
@@ -160,8 +160,8 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
     # kernel = NUTS(model, init_strategy=init_to_value(values=init_new), target_accept_prob=0.70)
     kernel = NUTS(model, 
                   init_strategy=init_to_feasible(), 
-                  target_accept_prob=0.70,
-                  max_tree_depth=1)
+                  target_accept_prob=0.80,
+                  max_tree_depth=12)
     mcmc = MCMC(
         kernel,
         num_warmup=NUM_WARMUP,
