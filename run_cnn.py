631045fa8caf9d6b80beae520cbaa3dba0c03f2b
Jamal <bajwa.jamal@gmail.com>
2022-02-10 19:21:30 -0500

    added new arch

--- run_cnn.py
+++ run_cnn.py
@@ -92,7 +92,7 @@ def run_conv_bnn(train_index=50000, num_warmup=100, num_samples=100, gpu=False):
             x = nn.avg_pool(x, window_shape=(3, 3), strides=(2, 2))
             x = nn.Conv(features=16, kernel_size=(3, 3))(x)
             x = nn.softplus(x)
-            x = nn.avg_pool(x, window_shape=(3, 3), strides=(2 2))
+            x = nn.avg_pool(x, window_shape=(3, 3), strides=(2, 2))
             x = x.reshape((x.shape[0], -1))  # flatten
             x = nn.Dense(features=64)(x)
             x = nn.softplus(x)
